d672541fd912aecfe095031b2519ebb972f6b441
Jon Bannon <jbannon@siena.edu>
2026-06-08 12:37:47 -0400

    Included the rough definitions.

--- SpectralTriples/Basic.lean
+++ SpectralTriples/Basic.lean
@@ -1,1 +1,47 @@
-def hello := "world"
+/-
+Copyright (c) 2026 Jon Bannon. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Jon Bannon, Michael R. Douglas
+-/
+
+module
+
+public import Mathlib.Analysis.InnerProductSpace.LinearPMap
+public import Mathlib.Order.CompletePartialOrder
+
+/-! # Spectral triples
+
+In this file we formalize spectral triples.
+
+## Main definitions
+
+* OddSpectralTriple
+* EvenSpectralTriple
+
+-/
+
+@[expose] public section
+
+open LinearPMap StarAlgebra ENNReal
+
+open ContinuousLinearMap LinearMap in
+structure OddSpectralTriple (A H : Type*) {𝕜 : Type*} [RCLike 𝕜] [Semiring A] [StarRing A]
+    [Algebra 𝕜 A] [NormedAddCommGroup H] [InnerProductSpace 𝕜 H] [CompleteSpace H]
+    (D : H →ₗ.[𝕜] H) (hD : IsSelfAdjoint D) where
+  π : StarAlgHom 𝕜 A (H →L[𝕜] H)
+  dom_comp (a : A) (x : D.domain) : π a x ∈ D.domain
+  comm (a : A) : iSup fun (x : Metric.closedBall (0 : D.domain) 1) ↦
+    ‖(π a) (D x) - (D ⟨(π a x), dom_comp a x⟩)‖ₑ < ∞
+
+open ContinuousLinearMap LinearMap in
+structure EvenSpectralTriple (A H : Type*) {𝕜 : Type*} [RCLike 𝕜] [Semiring A] [StarRing A]
+    [Algebra 𝕜 A] [NormedAddCommGroup H] [InnerProductSpace 𝕜 H] [CompleteSpace H]
+    (D : H →ₗ.[𝕜] H) (hD : IsSelfAdjoint D) (γ : H →L[𝕜] H) (hγS : IsSelfAdjoint γ)
+    (hγU : γ ∈ unitary (H →L[𝕜] H)) where
+  π : StarAlgHom 𝕜 A (H →L[𝕜] H)
+  dom_comp (a : A) (x : D.domain) : π a x ∈ D.domain
+  comm (a : A) : iSup fun (x : Metric.closedBall (0 : D.domain) 1) ↦
+    ‖(π a) (D x) - (D ⟨(π a x), dom_comp a x⟩)‖ₑ < ∞
+  grading_comm (a : A) : γ.comp (π a) = (π a).comp γ
+  grading_dom (x : D.domain) : γ x ∈ D.domain
+  grading (x : D.domain) : D ⟨γ x, grading_dom x⟩ = - γ (D x)
