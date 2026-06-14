ae12a9958ec83ee54943de12b55a16b5526b5e66
Jon Bannon <jbannon@siena.edu>
2026-06-14 18:18:11 -0400

    Small modification. Still considering whether this would be good as `Prop` valued.

--- SpectralTriples/Basic.lean
+++ SpectralTriples/Basic.lean
@@ -25,19 +25,19 @@ In this file we formalize spectral triples.
 open LinearPMap StarAlgebra ENNReal
 
 open ContinuousLinearMap LinearMap in
-structure OddSpectralTriple (A H : Type*) {𝕜 : Type*} [RCLike 𝕜] [Semiring A] [StarRing A]
-    [Algebra 𝕜 A] [NormedAddCommGroup H] [InnerProductSpace 𝕜 H] [CompleteSpace H]
-    (D : H →ₗ.[𝕜] H) (hD : IsSelfAdjoint D) where
-  π : StarAlgHom 𝕜 A (H →L[𝕜] H)
+structure IsOddSpectralTriple (A : Type*) {H : Type*} {𝕜 : Type*} [RCLike 𝕜] [Semiring A]
+    [StarRing A] [Algebra 𝕜 A] [NormedAddCommGroup H] [InnerProductSpace 𝕜 H] [CompleteSpace H]
+    (D : H →ₗ.[𝕜] H) (hD : IsSelfAdjoint D) (π : StarAlgHom 𝕜 A (H →L[𝕜] H)) where
   dom_comp (a : A) (x : D.domain) : π a x ∈ D.domain
   comm (a : A) : iSup fun (x : Metric.closedBall (0 : D.domain) 1) ↦
     ‖(π a) (D x) - (D ⟨(π a x), dom_comp a x⟩)‖ₑ < ∞
 
 open ContinuousLinearMap LinearMap in
-structure EvenSpectralTriple (A H : Type*) {𝕜 : Type*} [RCLike 𝕜] [Semiring A] [StarRing A]
-    [Algebra 𝕜 A] [NormedAddCommGroup H] [InnerProductSpace 𝕜 H] [CompleteSpace H]
-    (D : H →ₗ.[𝕜] H) (hD : IsSelfAdjoint D) (γ : H →L[𝕜] H) (hγS : IsSelfAdjoint γ)
-    (hγU : γ ∈ unitary (H →L[𝕜] H)) extends OddSpectralTriple A H D hD where
+structure IsEvenSpectralTriple (A : Type*) {H : Type*} {𝕜 : Type*} [RCLike 𝕜] [Semiring A]
+    [StarRing A] [Algebra 𝕜 A] [NormedAddCommGroup H] [InnerProductSpace 𝕜 H] [CompleteSpace H]
+    (D : H →ₗ.[𝕜] H) (hD : IsSelfAdjoint D) (π : StarAlgHom 𝕜 A (H →L[𝕜] H)) (γ : H →L[𝕜] H)
+    (hγS : IsSelfAdjoint γ) (hγU : γ ∈ unitary (H →L[𝕜] H))
+    extends IsOddSpectralTriple A D hD π where
   grading_comm (a : A) : γ.comp (π a) = (π a).comp γ
   grading_dom (x : D.domain) : γ x ∈ D.domain
   grading (x : D.domain) : D ⟨γ x, grading_dom x⟩ = - γ (D x)
